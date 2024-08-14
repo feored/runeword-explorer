702a36a64a71130317b86cef3af7441f6d694ad7
feored <feordo@gmail.com>
2024-08-14 20:06:13 +0200

    display different stat lists as tabs

--- src/lib/components/table/cols/Stats.svelte
+++ src/lib/components/table/cols/Stats.svelte
@@ -4,6 +4,7 @@
 	}
 	let { stats }: Stats = $props();
 	let single_stat: boolean = $derived.by(() => {return Object.keys(stats).length === 1 && 'all' in stats});
+	let active_tab = $state(0);
 </script>
 
 {#snippet stat_line_display(stat)}
@@ -15,18 +16,30 @@
 	<br />
 {/snippet}
 
+{#snippet stat_list_display(stats)}
+	{#each stats as stat}
+		{@render stat_line_display(stat)}
+	{/each}
+{/snippet}
+
+{#snippet stat_tabs(stats)}
+	<div role="tabs">
+		{#each Object.keys(stats) as stat_list_key, index}
+			<button class={active_tab == index ? "active" : "inactive outline reset"} type="button" onclick={() => active_tab = index}>{stat_list_key}</button>
+		{/each}
+  	</div>
+	{#each Object.keys(stats) as stat_list_key, index}
+		<section hidden={active_tab !== index}>
+			{@render stat_list_display(stats[stat_list_key])}
+		</section>
+	{/each}
+{/snippet}
+
 <div class="stats">
 	{#if single_stat}
-		{#each stats['all'] as stat}
-			{@render stat_line_display(stat)}
-		{/each}
+		{@render stat_list_display(stats['all'])}
 	{:else}
-		{#each Object.keys(stats) as statListKey}
-			<h4>{statListKey}</h4>
-			{#each stats[statListKey] as stat}
-				{@render stat_line_display(stat)}
-			{/each}
-		{/each}
+		{@render stat_tabs(stats)}
 	{/if}
 </div>
 
@@ -38,4 +51,19 @@
 	.variable {
 		color: #86b0ff;
 	}
+
+	[role="tabs"]{
+		display:flex;
+		justify-content: center;
+		gap: var(--pico-spacing);
+	}
+
+	[role="tabs"] button.active {
+		text-decoration: var(--pico-primary) underline;
+	}
+
+	[role="tabs"] button:not(.active) {
+		color: var(--pico-secondary) !important;
+	}
+
 </style>
---
+++ src/lib/components/ui/Tabs.svelte
@@ -0,0 +1,74 @@
+<nav role="tab-control">
+	<ul>
+	  <li><label for="tab1">Tab 1</label></li>
+	  <li><label for="tab2">Tab 2</label></li>
+	  <li><label for="tab3">Tab 3</label></li>
+	</ul>
+  </nav>
+  <div role="tabs">
+	<section>
+	  <input hidden="hidden" type="radio" name="tabs" id="tab1" checked="checked" />
+	  <figure>
+		<blockquote>
+		  "When you're new to something, you bring an ignorance that can
+		  be highly innovative."
+		  <footer>
+			<cite>– Rick Rubin</cite>
+		  </footer>
+		</blockquote>
+	  </figure>
+
+	  <input hidden="hidden" type="radio" name="tabs" id="tab2" />
+
+	  <figure>
+		<blockquote>
+		  "Nothing beats a simple worldview. When we know who is the bad
+		  guy, the day has structure."
+		  <footer>
+			<cite>– Volker Pispers</cite>
+		  </footer>
+		</blockquote>
+	  </figure>
+
+	  <input hidden="hidden" type="radio" name="tabs" id="tab3" />
+
+	  <figure>
+		<blockquote>
+		  "Ignorance is not bliss. Ignorance is poverty. Ignorance is devastation.
+		  Ignorance is tragedy. And ignorance is illness. It all stems from ignorance."
+		  <footer>
+			<cite>– Jim Rohn</cite>
+		  </footer>
+		</blockquote>
+	  </figure>
+	</section>
+  </div>
+
+<style>
+	[role="tabs"] {
+  display: flex;
+}
+
+[role="tabs"] section {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+[role="tabs"] figure {
+  flex-grow: 1;
+  width: 100%;
+  height: 100%;
+  display: none;
+}
+
+[role="tabs"] [type="radio"]:checked + figure {
+  display: block;
+}
+
+nav[role="tab-control"] label.active {
+  color: var(--primary);
+  cursor: pointer;
+}
+
+</style>
